214ffef21ab0a549e3809a9690303bb87d7fb31b
shalash23 <103309490+shalash23@users.noreply.github.com>
2023-01-13 22:53:59 +0200

    adjusting the background layer

---
+++ assets/index.js
@@ -0,0 +1,60 @@
+import geologyEarth from "./geology-earth.png";
+import geologyJupiter from "./geology-jupiter.png";
+import geologyMercury from "./geology-mercury.png";
+import geologyVenus from "./geology-venus.png";
+import geologyMars from "./geology-mars.png";
+import geologyUranus from "./geology-uranus.png";
+import geologySaturn from "./geology-saturn.png";
+import geologyNeptune from "./geology-neptune.png";
+import planetEarthInternal from "./planet-earth-internal.svg";
+import planetEarth from "./planet-earth.svg";
+import planetJupiter from "./planet-jupiter.svg";
+import planetJupiterInternal from "./planet-jupiter-internal.svg";
+import planetMarsInternal from "./planet-mars-internal.svg";
+import planetMars from "./planet-mars.svg";
+import planetMercury from "./planet-mercury.svg";
+import planetMercuryInternal from "./planet-mercury-internal.svg";
+import planetVenus from "./planet-venus.svg";
+import planetVenusInternal from "./planet-venus-internal.svg";
+import planetNeptune from "./planet-neptune.svg";
+import planetSaturn from "./planet-saturn.svg";
+import planetSaturnInternal from "./planet-saturn-internal.svg";
+import planetUranus from "./planet-uranus.svg";
+import planetUranusInternal from "./planet-uranus-internal.svg";
+import planetNeptuneInternal from "./planet-neptune-internal.svg";
+import iconChevron from "./icon-chevron.svg";
+import iconSource from "./icon-source.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import backgroundStars from "./background-stars.svg";
+
+export default {
+  backgroundStars,
+  geologyEarth,
+  geologyJupiter,
+  geologyMars,
+  geologyMercury,
+  geologyNeptune,
+  geologySaturn,
+  geologyNeptune,
+  geologyVenus,
+  planetEarth,
+  planetMars,
+  planetMercury,
+  planetUranus,
+  planetJupiter,
+  planetSaturn,
+  planetVenus,
+  planetNeptune,
+  planetEarthInternal,
+  planetNeptuneInternal,
+  planetJupiterInternal,
+  planetMarsInternal,
+  planetSaturnInternal,
+  planetUranusInternal,
+  planetVenusInternal,
+  planetMercuryInternal,
+  iconChevron,
+  iconSource,
+  iconHamburger,
+  geologyUranus,
+};
