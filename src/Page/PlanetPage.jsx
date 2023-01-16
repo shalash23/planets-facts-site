d2b8e16b8b41b24ab42af1588004c4c4ab3c7856
shalash23 <shalashmohamed232@outlook.com>
2023-01-16 16:25:32 +0200

    comitting the animation modification

--- src/Page/PlanetPage.jsx
+++ src/Page/PlanetPage.jsx
@@ -5,12 +5,18 @@ import images from "../../assets/index";
 import { planetContext } from "../Store/PlanetsContext";
 
 export default function PlanetPage({ location }) {
-  const planets = useContext(planetContext);
+  const planets = useContext(planetContext)[0];
 
   const locationObj =
     typeof location === "string"
       ? location
       : location.pathname.replace("/", "");
+  const planetImage = `planet${locationObj}`;
+
+  const planetProp = planets.filter((planet) => {
+    if (planet.name === locationObj) return planet;
+  })[0];
+  console.log(images["planetEarth"]);
 
   return (
     <Fragment>
@@ -20,25 +26,61 @@ export default function PlanetPage({ location }) {
           py: 8,
         }}
       >
-        <AnimatePresence mode="wait">
-          <Grid container spacing={4}>
-            <Grid item xs={8}>
+        <AnimatePresence>
+          <Grid
+            container
+            spacing={6}
+            component={motion.div}
+            sx={{
+              outline: "1px solid green",
+            }}
+          >
+            <Grid item xs={7}>
               <motion.img
-                src={images.planetEarth}
-                alt={`${locationObj} Picture`}
-                animate={{ rotate: 360 }}
+                src={images[planetImage]}
+                alt={`Planet ${locationObj} Picture`}
+                style={{
+                  width: "100%",
+                  objectFit: "fill",
+                }}
+                initial={{
+                  x: 50,
+                  scale: 0,
+                }}
+                animate={{ rotate: 360, x: 0, scale: 1 }}
                 transition={{
-                  repeat: Infinity,
-                  duration: 25,
-                  ease: "linear",
+                  rotate: { repeat: Infinity, duration: 25 },
+                  scale: { duration: 1, repeat: 0 },
+                }}
+                exit={{
+                  opacity: 0,
+                  x: -200,
+                  scale: 0.4,
                 }}
               />
             </Grid>
-            <Grid item xs={4}>
-              <Typography component="h1" variant="h2">
-                {locationObj}
+            <Grid
+              item
+              xs={5}
+              sx={{
+                outline: "1px solid green",
+              }}
+            >
+              <Typography
+                component={motion.h1}
+                variant="h2"
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 2,
+                }}
+              >
+                {planetProp.name}
               </Typography>
-              <Typography component="p" variant="subtitle2"></Typography>
+              <Typography component="p" variant="subtitle2">
+                {planetProp.overview.content}
+              </Typography>
             </Grid>
           </Grid>
         </AnimatePresence>
